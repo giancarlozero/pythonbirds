ce7dce621874d718389c4a97d833e52f5b30d825
Giancarlo Silva <giancarlozero@gmail.com>
2018-07-08 18:26:28 -0300

    Criado um exemplo de atributo complexo (atributo filhos).

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,24 @@
 class Pessoa:
     # Atributos
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
+        # Atributo complexo 'filhos'
+        self.filhos = list(filhos)
 
     # Método cumprimentar
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.nome, p.idade)
-    p.nome = 'Nataly'
-    p.idade = 24
-    print(p.nome, p.idade)
+    # Objeto comum - instância da classe Pessoa
+    filho = Pessoa(nome='Giancarlo', idade=34)
+    # Objeto com atributo complexo
+    mae = Pessoa(filho, nome='Bernadete', idade=18)
+
+    print(filho.nome)
+    print(mae.nome)
+    print()
+
+    for filho in mae.filhos:
+        print(filho.nome)
