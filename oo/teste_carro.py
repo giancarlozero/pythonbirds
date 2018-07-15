162b37e59baebfdd2a5207791c102987e84c2d0d
Giancarlo Silva <giancarlozero@gmail.com>
2018-07-14 22:09:36 -0300

    Exercício sobre testes unitários.

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -1,9 +1,17 @@
 from unittest import TestCase
 
-from oo.carro import Motor
+from oo.carro import Motor, Direcao, Carro
 
 
 class CarroTestCase(TestCase):
+    def teste_direcao(self):
+        direcao = Direcao()
+        direcao.girar_a_esquerda()
+        self.assertEqual(None, direcao.girar_a_esquerda())
+
+        direcao.girar_a_direita()
+        self.assertEqual(None, direcao.girar_a_direita())
+
     def teste_velocidade_inicial(self):
         motor = Motor()
         self.assertEqual(0, motor.velocidade)
@@ -12,3 +20,8 @@ class CarroTestCase(TestCase):
         motor = Motor()
         motor.acelerar()
         self.assertEqual(1, motor.velocidade)
+
+    def teste_frear(self):
+        motor = Motor()
+        motor.frear()
+        self.assertEqual(0, motor.velocidade)
